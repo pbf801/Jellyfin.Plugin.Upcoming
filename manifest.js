89618bad6795112dc4ce9eed2405e88787ab5a29
pbf801 <76563785+pbf801@users.noreply.github.com>
2026-08-17 15:23:39 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+  "guid": "6d8c0b3d-0e54-4c1a-91d2-4c0b2d6a8a71",
+  "name": "Jellyfin Upcoming",
+  "description": "Manually managed upcoming releases for Jellyfin",
+  "overview": "Adds upcoming content to Jellyfin.",
+  "owner": "TU_USUARIO",
+  "category": "General",
+  "versions": []
+}
